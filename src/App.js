79fd405353c4ee2744e0c180a26bfd02ead3cc9d
Ryan Lemon <ryanalemon@gmail.com>
2023-07-21 08:32:02 -0700

    some initial testing for generation logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import './App.css';
 
+// Function to console log a 2D array
+// Mainly for temporary use while I figure out crossword generation logic
+// @param table: 2d array
+// @return finalString: string representation of table
+const renderTable = (table) => {
+  let finalString = "";
+  for (let row = 0; row < table.length; row++) {
+    for (let col = 0; col < table[row].length; col++) {
+      finalString += (table[row][col] + " ");
+    }
+    finalString += "\n";
+  }
+  return finalString;
+}
+
+// Given an array of strings, generate a crossword represented as a 2d array
+// @param stringArr: array of strings
+// @param size: int, size of the array
+// @return crossword: 2d array of characters
+const generateCrossword = (stringArr, size) => {
+  
+}
+
 function App() {
+  const testTable = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"]
+  ];
+  const testTableString = renderTable(testTable);
+  console.log(testTableString);
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          Console logging for now
+        </div>
       </header>
     </div>
   );
